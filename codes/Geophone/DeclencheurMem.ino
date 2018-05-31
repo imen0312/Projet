51f6c374467ae3e3b89a64761be361132c95950f
imen0312 <34740927+imen0312@users.noreply.github.com>
2018-05-31 11:16:41 +0200

    Create DeclencheurMem.ino

---
+++ codes/Geophone/DeclencheurMem.ino
@@ -0,0 +1,90 @@
+#include <SD.h>
+#include <SPI.h>
+
+
+
+
+const int geophone = A0;
+
+int chipSelect = 4; //chip select pin for the microSD Card Adapter
+float sensorValue;
+float value;
+File fich; // file object that is used to read and write data
+float valueMin=2.90;
+long time=0;
+
+
+void setup() {
+
+  Serial.begin(9600); // start serial connection to print our debug messages and data
+
+  pinMode(chipSelect, OUTPUT); // chip select pin must be set to OUTPUT mode
+
+
+  while (!SD.begin(chipSelect)) { // initialize SD card
+
+    Serial.println("Could not initialize SD card."); // if return value is false, something went wrong.
+    delay(1000);
+
+  }
+
+
+
+}
+
+
+
+void loop(){
+  value=analogRead(geophone);
+  sensorValue=value*5/1023;
+  Serial.print(sensorValue);
+  fich =SD.open("fich3.txt", FILE_WRITE); // open "fich.txt" to write data
+  if (fich){
+    Serial.println("fich open");
+    if(sensorValue <=valueMin){
+      Serial.println("Valeur minimale atteinte");
+      time=millis();
+      while((millis()-time)<60000.00){
+        fich.println(analogRead(geophone)*(5.0/1023.0)); // write geophone's values to file
+        sensorValue=(analogRead(geophone)*(5.0/1023.0));
+        Serial.print("Wrote value:"); // debug output: show written number in serial monitor
+        Serial.println(sensorValue);
+      }
+      Serial.println("End");
+    }
+    fich.close(); // close file
+  }
+    
+  else {
+
+    Serial.print("Could not open fich (writing).");
+
+  }
+
+
+//File file= SD.open("FICH.txt", FILE_READ); // open "fich.txt" to read data
+//if(file) {
+//
+//    Serial.println("--- Reading start ---");
+//
+//     char character;
+//
+//     while((character=file.read()) != -1) { // this while loop reads data stored in 'file.txt" and prints it to serial monitor
+//
+//      Serial.print(character);
+//    }
+//
+//    file.close();
+//
+//    Serial.println("--- Reading end ---");
+//
+//  } else {
+//
+//    Serial.println("Could not open file (reading).");
+//
+//  }
+//
+//  delay(5000); // wait for 5000ms
+
+
+}
